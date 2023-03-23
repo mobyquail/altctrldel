1e51f00b68266072202a2d108ec26a143cc5789d
mobyquail <126684648+mobyquail@users.noreply.github.com>
2023-03-22 23:51:25 -0400

    Add files via upload

---
+++ write_Logdate.ps1
@@ -0,0 +1,5 @@
+﻿#$Currentpath = ("C:\OOHacks") 
+$Currentpath = split-path -parent $MyInvocation.MyCommand.Definition
+$FileLogdate = Get-Date -format 'dd.MM.yyyy HH_mm_ss'
+Write-Host($FileLogdate)
+New-Item -Path $Currentpath -Name "$FileLogdate" -ItemType "file" | Rename-Item -newname {"DC-Down-" + $_.name + ".txt"}
